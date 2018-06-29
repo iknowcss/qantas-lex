df39712d42dd2ec7558803d54ca12747ec2c5dde
Cody Jenkins <cjenkins@dius.com.au>
2018-06-29 11:41:18 +1000

    Initial FindRestaurants intent

--- src/index.js
+++ src/index.js
@@ -1,23 +1,136 @@
-function elicitIntent(message, options) {
+/// - Util -----------------------------------------------------------------------------------------
 
+function elicitSlot(sessionAttributes, intentName, slots, slotToElicit, message, responseCard) {
+  return {
+    sessionAttributes,
+    dialogAction: {
+      type: 'ElicitSlot',
+      intentName,
+      slots,
+      slotToElicit,
+      message,
+      responseCard,
+    },
+  };
 }
 
-exports.handler = async (event) => {
-  console.log('Lex request event', event);
+function elicitIntent(sessionAttributes, intentName, slots, slotToElicit, message, responseCard) {
+  return {
+    sessionAttributes,
+    dialogAction: {
+      type: 'ElicitIntent',
+      intentName,
+      slots,
+      slotToElicit,
+      message,
+      responseCard,
+    },
+  };
+}
+
+function confirmIntent(sessionAttributes, intentName, slots, message, responseCard) {
+  return {
+    sessionAttributes,
+    dialogAction: {
+      type: 'ConfirmIntent',
+      intentName,
+      slots,
+      message,
+      responseCard,
+    },
+  };
+}
+
+function close(sessionAttributes, fulfillmentState, message, responseCard) {
+  return {
+    sessionAttributes,
+    dialogAction: {
+      type: 'Close',
+      fulfillmentState,
+      message,
+      responseCard,
+    },
+  };
+}
+
+function delegate(sessionAttributes, slots) {
+  return {
+    sessionAttributes,
+    dialogAction: {
+      type: 'Delegate',
+      slots,
+    },
+  };
+}
+
+// --------------- Intents -------------------------------------------------------------------------
+
+exports.findSpendIdeas = function findSpendIdeas(intentRequest) {
+  const { IdeaType } = intentRequest.currentIntent.slots;
+  const source = intentRequest.invocationSource;
+  const outputSessionAttributes = intentRequest.sessionAttributes || {};
+  // const bookingMap = JSON.parse(outputSessionAttributes.bookingMap || '{}');
+
   return {
     "dialogAction": {
-      "type": "ElicitSlot",
+      "type": "ElicitIntent",
       "message": {
         "contentType": "PlainText",
         "content": "What kind of restaurant are you looking for?"
       },
-      "intentName": "FindRestaurant",
-      "slots": {
-        "Cuisine": null,
-        "Price": null,
-        "Suburb": null
-      },
-      "slotToElicit" : "Cuisine"
+      "intentName": "FindRestaurant"
     }
   };
+}
+
+exports.findRestaurants = function findRestaurants(intentRequest) {
+  const slots = intentRequest.currentIntent.slots;
+  const source = intentRequest.invocationSource;
+  const sessionAttributes = intentRequest.sessionAttributes || {};
+
+  if (!slots.Cuisine || !slots.Price || !slots.Suburb) {
+    return {
+      sessionAttributes,
+      dialogAction: {
+        type: 'Delegate',
+        slots: { Cuisine: null, Suburb: null, Price: null },
+      }
+    };
+  }
+
+  return {
+    sessionAttributes: {},
+    dialogAction: {
+      type: 'Close',
+      fulfillmentState: 'Fulfilled',
+      message: {
+        contentType: 'PlainText',
+        content: `I found 3 ${slots.Price} ${slots.Cuisine} restaurants in ${slots.Suburb}! Here they are:`
+      },
+    },
+  };
+};
+
+function routeIntentRequest(intentRequest) {
+  console.log(`dispatch userId=${intentRequest.userId}, intent=${intentRequest.currentIntent.name}`);
+
+  const name = intentRequest.currentIntent.name;
+
+  // Dispatch to your skill's intent handlers
+  switch (name) {
+    case 'HowSpend':
+      return findSpendIdeas(intentRequest);
+    case 'FindRestaurants':
+      return findRestaurants(intentRequest);
+    default:
+      throw new Error(`Intent "${name}" not supported`);
+  }
+}
+
+/// - Entry ----------------------------------------------------------------------------------------
+
+exports.handler = async (event) => {
+  console.log('Lex request event', event);
+
+  return routeIntentRequest(event);
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,76 +1,161 @@
-const index = require('./index');
+const { findRestaurants } = require('./index');
 
 describe('lambda entry', () => {
-  it('does something', async () => {
-    const responseJson = await expect(index.handler({
-      messageVersion: '1.0',
-      invocationSource: 'DialogCodeHook',
-      // userId: 'esgtzomeyplw2nstkzgy1pd2iuoxiwuc',
-      sessionAttributes: {},
-      requestAttributes: null,
-      bot: { name: 'SpendQantasPoints',
-        alias: '$LATEST',
-        version: '$LATEST'
-      },
-      outputDialogMode: 'Text',
-      currentIntent: {
-        name: 'FindRestaurant',
-        slots: { Cuisine: null, Suburb: null, Price: null },
-        // slotDetails: { Cuisine: [Object], Suburb: [Object], Price: [Object] },
-        confirmationStatus: 'None'
-      },
-      inputTranscript: 'How can I spend my Qantas points'
-    })).to.be.fulfilled;
+  xdescribe('HowSpend intent', () => {
+    it('handles a request with no slots', () => {
+      const result = findRestaurants({
+        messageVersion: '1.0',
+        invocationSource: 'DialogCodeHook',
+        userId: 'mock-user-id',
+        sessionAttributes: {},
+        requestAttributes: null,
+        bot: {
+          name: 'SpendQantasPoints',
+          alias: '$LATEST',
+          version: '$LATEST'
+        },
+        outputDialogMode: 'Text',
+        currentIntent: {
+          name: 'FindRestaurant',
+          slots: { Cuisine: null, Suburb: null, Price: null },
+          confirmationStatus: 'None'
+        },
+        inputTranscript: 'What restaurants can I spend points at?'
+      });
 
-    expect(JSON.parse(responseJson)).to.eql({
-      "dialogState": "ElicitSlot",
-      "intentName": "FindRestaurant",
-      "message": "What kind of food are you looking for?",
-      "messageFormat": "PlainText",
-      "responseCard": null,
-      "sessionAttributes": {},
-      "slotToElicit": "Cuisine",
-      "slots": {
-        "Cuisine": null,
-        "Price": null
-      }
+      expect(result).to.eql({
+
+      });
     });
   });
 
-  it('does something else', async () => {
-    const responseJson = await expect(index.handler({
-      messageVersion: '1.0',
-      invocationSource: 'DialogCodeHook',
-      // userId: 'esgtzomeyplw2nstkzgy1pd2iuoxiwuc',
-      sessionAttributes: {},
-      requestAttributes: null,
-      bot: { name: 'SpendQantasPoints',
-        alias: '$LATEST',
-        version: '$LATEST'
-      },
-      outputDialogMode: 'Text',
-      currentIntent: {
-        name: 'FindRestaurant',
-        slots: { Cuisine: null, Suburb: null, Price: null },
-        // slotDetails: { Cuisine: [Object], Suburb: [Object], Price: [Object] },
-        confirmationStatus: 'None'
-      },
-      inputTranscript: 'How can I spend my Qantas points'
-    })).to.be.fulfilled;
+  describe('FindRestaurants intent', () => {
+    function buildFindRestaurantsRequest(options) {
+      const slots = Object.assign({ Cuisine: null, Suburb: null, Price: null }, options.slots);
+      return {
+        messageVersion: '1.0',
+        invocationSource: 'DialogCodeHook',
+        userId: 'mock-user-id',
+        sessionAttributes: {},
+        requestAttributes: null,
+        bot: {
+          name: 'SpendQantasPoints',
+          alias: '$LATEST',
+          version: '$LATEST'
+        },
+        outputDialogMode: 'Text',
+        currentIntent: {
+          name: 'FindRestaurant',
+          slots,
+          confirmationStatus: 'None'
+        },
+        inputTranscript: 'What restaurants can I spend points at?'
+      };
+    }
 
-    expect(JSON.parse(responseJson)).to.eql({
-      "dialogState": "ReadyForFulfillment",
-      "intentName": "FindRestaurant",
-      "message": null,
-      "messageFormat": null,
-      "responseCard": null,
-      "sessionAttributes": {},
-      "slotToElicit": null,
-      "slots": {
-        "Cuisine": "Thai",
-        "Price": "Casual",
-        "Suburb": "Coogee"
-      }
+    it('delegates a request with no slots', () => {
+      const result = findRestaurants(buildFindRestaurantsRequest({
+        slots: { Cuisine: null, Suburb: null, Price: null }
+      }));
+
+      expect(result).to.eql({
+        sessionAttributes: {},
+        dialogAction: {
+          type: 'Delegate',
+          slots: { Cuisine: null, Suburb: null, Price: null },
+        },
+      });
+    });
+
+    it('fulfills a request with all slots filled', () => {
+      const result = findRestaurants(buildFindRestaurantsRequest({
+        slots: { Cuisine: 'Thai', Suburb: 'Newtown', Price: 'cheap' }
+      }));
+
+      expect(result).to.eql({
+        sessionAttributes: {},
+        dialogAction: {
+          type: 'Close',
+          fulfillmentState: 'Fulfilled',
+          message: {
+            contentType: 'PlainText',
+            content: 'I found 3 cheap Thai restaurants in Newtown! Here they are:'
+          },
+        },
+      });
     });
   });
+
+  // it('does something', async () => {
+  //   const responseJson = await expect(index.handler({
+  //     messageVersion: '1.0',
+  //     invocationSource: 'DialogCodeHook',
+  //     // userId: 'esgtzomeyplw2nstkzgy1pd2iuoxiwuc',
+  //     sessionAttributes: {},
+  //     requestAttributes: null,
+  //     bot: { name: 'SpendQantasPoints',
+  //       alias: '$LATEST',
+  //       version: '$LATEST'
+  //     },
+  //     outputDialogMode: 'Text',
+  //     currentIntent: {
+  //       name: 'FindRestaurant',
+  //       slots: { Cuisine: null, Suburb: null, Price: null },
+  //       // slotDetails: { Cuisine: [Object], Suburb: [Object], Price: [Object] },
+  //       confirmationStatus: 'None'
+  //     },
+  //     inputTranscript: 'How can I spend my Qantas points'
+  //   })).to.be.fulfilled;
+  //
+  //   expect(JSON.parse(responseJson)).to.eql({
+  //     "dialogState": "ElicitSlot",
+  //     "intentName": "FindRestaurant",
+  //     "message": "What kind of food are you looking for?",
+  //     "messageFormat": "PlainText",
+  //     "responseCard": null,
+  //     "sessionAttributes": {},
+  //     "slotToElicit": "Cuisine",
+  //     "slots": {
+  //       "Cuisine": null,
+  //       "Price": null
+  //     }
+  //   });
+  // });
+  //
+  // it('does something else', async () => {
+  //   const responseJson = await expect(index.handler({
+  //     messageVersion: '1.0',
+  //     invocationSource: 'DialogCodeHook',
+  //     // userId: 'esgtzomeyplw2nstkzgy1pd2iuoxiwuc',
+  //     sessionAttributes: {},
+  //     requestAttributes: null,
+  //     bot: { name: 'SpendQantasPoints',
+  //       alias: '$LATEST',
+  //       version: '$LATEST'
+  //     },
+  //     outputDialogMode: 'Text',
+  //     currentIntent: {
+  //       name: 'FindRestaurant',
+  //       slots: { Cuisine: null, Suburb: null, Price: null },
+  //       // slotDetails: { Cuisine: [Object], Suburb: [Object], Price: [Object] },
+  //       confirmationStatus: 'None'
+  //     },
+  //     inputTranscript: 'How can I spend my Qantas points'
+  //   })).to.be.fulfilled;
+  //
+  //   expect(JSON.parse(responseJson)).to.eql({
+  //     "dialogState": "ReadyForFulfillment",
+  //     "intentName": "FindRestaurant",
+  //     "message": null,
+  //     "messageFormat": null,
+  //     "responseCard": null,
+  //     "sessionAttributes": {},
+  //     "slotToElicit": null,
+  //     "slots": {
+  //       "Cuisine": "Thai",
+  //       "Price": "Casual",
+  //       "Suburb": "Coogee"
+  //     }
+  //   });
+  // });
 });
