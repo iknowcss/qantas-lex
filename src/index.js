ed0732e1eef8a34494e844c13a9ce00690ac51d9
Cody Jenkins <cjenkins@dius.com.au>
2018-06-29 10:37:48 +1000

    First working elicit slot

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ function elicitIntent(message, options) {
 
 exports.handler = async (event) => {
   console.log('Lex request event', event);
-  return JSON.stringify({
-    "dialogState": "ElicitSlot",
-    "intentName": "FindRestaurant",
-    "message": "What kind of food are you looking for?",
-    "messageFormat": "PlainText",
-    "responseCard": null,
-    "sessionAttributes": {},
-    "slotToElicit": "Cuisine",
-    "slots": {
-      "Cuisine": null,
-      "Price": null
+  return {
+    "dialogAction": {
+      "type": "ElicitSlot",
+      "message": {
+        "contentType": "PlainText",
+        "content": "What kind of restaurant are you looking for?"
+      },
+      "intentName": "FindRestaurant",
+      "slots": {
+        "Cuisine": null,
+        "Price": null,
+        "Suburb": null
+      },
+      "slotToElicit" : "Cuisine"
     }
-  });
+  };
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -36,4 +36,41 @@ describe('lambda entry', () => {
       }
     });
   });
+
+  it('does something else', async () => {
+    const responseJson = await expect(index.handler({
+      messageVersion: '1.0',
+      invocationSource: 'DialogCodeHook',
+      // userId: 'esgtzomeyplw2nstkzgy1pd2iuoxiwuc',
+      sessionAttributes: {},
+      requestAttributes: null,
+      bot: { name: 'SpendQantasPoints',
+        alias: '$LATEST',
+        version: '$LATEST'
+      },
+      outputDialogMode: 'Text',
+      currentIntent: {
+        name: 'FindRestaurant',
+        slots: { Cuisine: null, Suburb: null, Price: null },
+        // slotDetails: { Cuisine: [Object], Suburb: [Object], Price: [Object] },
+        confirmationStatus: 'None'
+      },
+      inputTranscript: 'How can I spend my Qantas points'
+    })).to.be.fulfilled;
+
+    expect(JSON.parse(responseJson)).to.eql({
+      "dialogState": "ReadyForFulfillment",
+      "intentName": "FindRestaurant",
+      "message": null,
+      "messageFormat": null,
+      "responseCard": null,
+      "sessionAttributes": {},
+      "slotToElicit": null,
+      "slots": {
+        "Cuisine": "Thai",
+        "Price": "Casual",
+        "Suburb": "Coogee"
+      }
+    });
+  });
 });
